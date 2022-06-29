c9acdf52450e2cec6b8b024870eaf472cbec5a80
Diye Mark <diyye101@gmail.com>
2022-06-29 11:05:07 +0300

    mlflow run

--- train.py
+++ train.py
@@ -6,14 +6,15 @@ path = "data/data.csv"
 repo = "https://github.com/DiyeMark/Causal-Inference"
 version = None
 
-# data_url = dvc.api.get_url(path=path, repo=repo, rev=version)
+data_url = dvc.api.get_url(path=path, repo=repo, rev=version)
+
+mlflow.set_experiment("Causal Inference")
 
 if __name__ == "__main___":
-    mlflow.set_experiment("Causal Inference")
 
-    # data = pd.read_csv(data_url, sep=",")
+    data = pd.read_csv(data_url, sep=",")
 
-    # mlflow.log_param("data_url", data_url)
-    # mlflow.log_param("data_version", version)
+    mlflow.log_param("data_url", data_url)
+    mlflow.log_param("data_version", version)
 
-    # mlflow.log_artifact("data/data.csv")
+    mlflow.log_artifact("data/data.csv")
