18c17f83ce4c9724aefefc72a0cf7c0d63310e74
Diye Mark <diyye101@gmail.com>
2022-07-03 07:22:48 +0300

    wip: causal inference

--- scripts/data_cleaner.py
+++ scripts/data_cleaner.py
@@ -2,16 +2,25 @@ import pandas as pd
 
 
 class DataCleaner:
-    def __init__(self, df: pd.DataFrame) -> None:
-        self.df = df
+    def __init__(self) -> None:
+        pass
 
-    def remove_unwanted_columns(self, columns: list) -> pd.DataFrame:
-        self.df.drop(columns, axis=1, inplace=True)
-        return self.df
+    def remove_unwanted_columns(self,  df: pd.DataFrame, columns: list) -> pd.DataFrame:
+        df.drop(columns, axis=1, inplace=True)
+        return df
 
-    def remove_nulls(self) -> pd.DataFrame:
-        return self.df.dropna()
+    def remove_nulls(self,  df: pd.DataFrame) -> pd.DataFrame:
+        return df.dropna()
 
-    def remove_duplicates(self) -> pd.DataFrame:
-        remove = self.df[self.df.duplicated()].index
-        return self.df.drop(index=remove, inplace=True)
+    def remove_duplicates(self,  df: pd.DataFrame) -> pd.DataFrame:
+        remove = df[df.duplicated()].index
+        return df.drop(index=remove, inplace=True)
+
+    def missing_percentage(self, df: pd.DataFrame):
+        percent_missing = df.isnull().sum() * 100 / len(df)
+        missing_value_df = pd.DataFrame(
+            {"column_name": df.columns, "percent_missing": percent_missing}
+        )
+        missing_value_df.reset_index(drop=True, inplace=True)
+        return missing_value_df
+
