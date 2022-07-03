6f480fc5f002f92285270e8d34e7bd3b79bfe33a
Diye Mark <diyye101@gmail.com>
2022-07-03 16:32:23 +0300

    init: test

---
+++ test/unit_test.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import unittest
+import pandas as pd
+
+
+class UnitTest(unittest.TestCase):
+
+    def setUp(self) -> pd.DataFrame:
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
